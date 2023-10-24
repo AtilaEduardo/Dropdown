3390a7386e28a157b8a48fdfc691426efc58a979
ThaisPio <89753258+ThaisPio@users.noreply.github.com>
2023-10-24 10:28:56 -0300

    Inclusão da pasta components e alteração em App.tsx e App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import { useRef, useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
+import { Down } from './Components/Index';
 
 function App() {
-  const [open, abrir] = useState<boolean>(false);
-  const botaoRef = useRef<HTMLDivElement>(null)
-  const estado_botao = (state: boolean) => {
-    abrir(!state);
-  };
-  const clicar_fora =(e:any)=>{
-    if(open && !botaoRef.current?.contains(e.target as Node)){
-      abrir(false)
-      
-    }
-  }
-  window.addEventListener("click",clicar_fora)
-  
-  console.log(open);
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <div className="App">
-      <div className="botao" ref={botaoRef}>
-        <button onClick={(e) => estado_botao(open)}>Selecione aluno</button>
-        
-        {open && (
-          <ul>
-            <li onClick={()=> console.log("Átila")}>Átila </li>
-            <li onClick={()=> console.log("Leonardo")}>Leonardo</li>
-            <li onClick={()=> console.log("Thais")}>Thais</li>
-            <li onClick={()=> console.log("Viviane")}>Viviane</li>
-          </ul>
-        )}
-      </div>
+    <div className='App'>
+      <Down Estado={open} setOpen={setOpen} />
     </div>
-  );
+  )
 }
 
 export default App;
